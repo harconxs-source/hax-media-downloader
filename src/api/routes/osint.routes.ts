b37adebcec0d9625df7c37c9b7c013ab34b20762
hax-tech <samhaxbilling@gmail.com>
2026-09-10 16:35:13 +0500

    Delete src directory

--- src/api/routes/osint.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { osintController } from '../controllers/osint.controller.ts';
-import { optionalUserAuthMiddleware } from '../../middleware/auth.middleware.ts';
-import { downloadRateLimiter } from '../../middleware/rate-limit.middleware.ts';
-
-const router = Router();
-router.post('/profile', optionalUserAuthMiddleware, downloadRateLimiter, (req, res, next) => osintController.profile(req, res, next));
-router.get('/profile', optionalUserAuthMiddleware, downloadRateLimiter, (req, res, next) => osintController.profile(req, res, next));
-export default router;
